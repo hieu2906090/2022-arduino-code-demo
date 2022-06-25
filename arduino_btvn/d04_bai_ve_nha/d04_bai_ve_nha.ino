5eceedf9bd846f26806a9728a37f2b9161dba890
Hieu <hieu2906090@gmail.com>
2022-06-25 18:02:50 +0700

    d04 add code btvn solved by using custom function

---
+++ arduino_btvn/d04_bai_ve_nha/d04_bai_ve_nha.ino
@@ -0,0 +1,43 @@
+byte ledPin[] = {2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+byte pinCount;
+
+// ################################################################################
+// I. CUSTOM DEV SECTION
+// ################################################################################
+void loopLedByMode(int soLanChay, int soTrungVi, byte mode, int delayTime) {
+    for (int i = 0; i <= soLanChay; i++) {
+        if (i == 0)
+            digitalWrite(ledPin[soTrungVi], mode);
+        else {
+            digitalWrite(ledPin[soTrungVi - i], mode);
+            digitalWrite(ledPin[soTrungVi + i], mode);
+        }
+        delay(delayTime);
+    }
+}
+
+void chayLedTuTrungVi(int doDaiMang) {
+    int soTrungVi = round(doDaiMang / 2);
+    int soLanChay = doDaiMang - soTrungVi;
+    // B1. Open led
+    loopLedByMode(soLanChay, soTrungVi, HIGH, 300);
+    // B2. Dim led
+    loopLedByMode(soLanChay, soTrungVi, HIGH, 300);
+}
+
+// ################################################################################
+// II. MAIN RUN SECTION
+// ################################################################################
+
+void setup() {
+    pinCount = sizeof(ledPin);
+
+    for (int i = 0; i < pinCount; i++) {
+        pinMode(ledPin[i], OUTPUT);
+        digitalWrite(ledPin[i], LOW);
+    }
+}
+
+// ---------------------------------------------------------------------------------
+void loop() { chayLedTuTrungVi(pinCount); }
